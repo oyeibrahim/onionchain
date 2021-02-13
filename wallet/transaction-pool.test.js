0760f81f6377fc8571508740bc29394192e97500
oyeibrahim <oyetunjiibrahim@gmail.com>
2021-02-13 13:18:38 +0100

    Transaction update frontend and test

--- wallet/transaction-pool.test.js
+++ wallet/transaction-pool.test.js
@@ -4,12 +4,13 @@ const Wallet = require('./index');
 
 
 describe('TransactionPool', () => {
-    let transactionPool, transaction;
+    let transactionPool, transaction, senderWallet;
 
     beforeEach(() => {
         transactionPool = new TransactionPool();
+        senderWallet = new Wallet();
         transaction = new Transaction({
-            senderWallet: new Wallet,
+            senderWallet,
             recipient: 'fake-recipient',
             amount: 50
         })
@@ -21,6 +22,17 @@ describe('TransactionPool', () => {
             transactionPool.setTransaction(transaction);
 
             expect(transactionPool.transactionMap[transaction.id])
+                .toBe(transaction);
+        })
+
+    })
+
+    describe('existingTransaction()', () => {
+
+        it('returns an existing transaction given an input address', () => {
+            transactionPool.setTransaction(transaction);
+
+            expect(transactionPool.existingTransaction({ inputAddress: senderWallet.publicKey }))
                 .toBe(transaction);
         })
 
