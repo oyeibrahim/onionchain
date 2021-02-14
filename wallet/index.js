e68ac39b86f22c6720e6c2498541dc662af71b30
oyeibrahim <oyetunjiibrahim@gmail.com>
2021-02-14 16:45:34 +0100

    Wallet Balance

--- wallet/index.js
+++ wallet/index.js
@@ -20,11 +20,50 @@ class Wallet {
         return this.keyPair.sign(cryptoHash(data));
     }
 
-    createTransaction({ recipient, amount }) {
+    createTransaction({ recipient, amount, chain }) {
+        if (chain) {
+            this.balance = Wallet.calculateBalance({
+                chain,
+                address: this.publicKey
+            });
+        }
+
         if (amount > this.balance) {
             throw new Error('Amount exceeds balance');
         }
         return new Transaction({ senderWallet: this, recipient, amount });
+    }
+
+    static calculateBalance({ chain, address }) {
+        //keeps track of if the wallet has conducted a transaction 
+        //in which case the calculation starts from the last transaction
+        let hasConductedTransaction = false;
+
+        let outputsTotal = 0;
+
+        //ends at 1 to omit genesis block
+        for (let i = chain.length - 1; i > 0; i--) {
+            const block = chain[i];
+
+            for (let transaction of block.data) {
+                if (transaction.input.address === address) {
+                    hasConductedTransaction = true;
+                }
+                const addressOutput = transaction.outputMap[address];
+
+                if (addressOutput) {
+                    outputsTotal = outputsTotal + addressOutput;
+                }
+            }
+
+            if (hasConductedTransaction) {
+                break;
+            }
+
+        }
+
+        return hasConductedTransaction ? outputsTotal : STARTING_BALANCE + outputsTotal;
+
     }
 
 }
