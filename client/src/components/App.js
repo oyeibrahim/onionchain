7fbf9e5c1a64c4b8c4aba1f09e2d240b017a1f7e
oyeibrahim <oyetunjiibrahim@gmail.com>
2021-02-21 00:36:03 +0100

    State setup

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react'
 
 class App extends Component {
+
+    state = {
+        walletInfo: { address: 'onionxv6', balance: 9999 }
+    };
+
     render() {
+
+        const { address, balance } = this.state.walletInfo;
+
         return (
             <div>
                 Welcome to Onion blockchain...
