d075dec4a61dabb6ada98643d951a7f35e4f795b
oyeibrahim <oyetunjiibrahim@gmail.com>
2021-02-23 20:30:43 +0100

    Scripts now platform specific

--- client/dist/src.e31bb0bc.js
+++ client/dist/src.e31bb0bc.js
@@ -47157,7 +47157,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60635" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60900" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
